5b1ecb31f2298c0082ca5ddef87a6ebe6b966bdc
joatop <joachim.topper@nina.no>
2024-11-17 10:20:41 +0100

    trying aggregation

--- R/gruk data handling.R
+++ R/gruk data handling.R
@@ -10,6 +10,53 @@ yyy <- yyy %>%
   mutate(ec = mean(c(fpi,aliens,erosion,shrub), na.rm=T))
 
 
+yyy$agg.ec <- NA
+for (i in 1:nrow(yyy) ) {
+  
+  dat <- as.vector(unlist(yyy[i,c("fpi","aliens","erosion","shrub")]))
+  
+  yyy[i,"agg.ec"] <- expit(summary(betareg(dat~1))$coefficients$mu[1])
+  
+  
+}
+
+
+betareg(as.vector(unlist(dat))~1)
+
+
+library(statmod)
+
+agg <- yyy %>%
+  rowwise() %>%
+  mutate(model = map(data, ~betareg(c(fpi,aliens,erosion,shrub)~1, data = .x))) %>%
+  mutate(tidy = map(model, tidy),
+         glance = map(model, glance),
+         augment = map(model, augment),
+         intercept = tidy %>% map_dbl(~ filter(.x, term == "(Intercept)") %>% pull(estimate)) ) %>%
+  dplyr::select(ID, intercept)
+
+
+
+# from ØT fjell
+# calculate linear regressions for each pixel
+# calculate linear regressions for each pixel
+ndviTrends <- ndviTS  %>% 
+  group_by(ID) %>% 
+  nest()%>% 
+  mutate(model = map(data, ~lm(ndvi ~ year, data = .x))) %>%
+  mutate(tidy = map(model, tidy),
+         glance = map(model, glance),
+         augment = map(model, augment),
+         rsq = glance %>% map_dbl('r.squared'),
+         pvalue = glance %>% map_dbl('p.value'),
+         intercept = tidy %>% map_dbl(~ filter(.x, term == "(Intercept)") %>% pull(estimate)),
+         slope = tidy %>% map_dbl(~ filter(.x, term == "year") %>% pull(estimate))) %>%
+  dplyr::select(ID, intercept, slope, rsq, pvalue)
+
+
+
+
+
 yyy %>%
   rowwise() %>%
   mod = betareg(c(fpi,aliens,erosion,shrub)~1) %>%
